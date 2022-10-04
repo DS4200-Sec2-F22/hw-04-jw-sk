47d0921f03255102473900cd9e6968b3ad8fe5b2
Jeffrey Wallace <jcwallace002@gmail.com>
2022-10-04 13:19:51 -0400

    v2

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+var counter = 4;
+
+document.getElementById("circle-1").addEventListener("click", function(){
+    showID("circle-1");});
+
+document.getElementById("circle-2").addEventListener("click", function(){
+    showID("circle-2");});
+
+document.getElementById("circle-3").addEventListener("click", function(){
+    showID("circle-3");});
+
+document.getElementById("circle-4").addEventListener("click", function(){
+    showID("circle-4");});
+
+
+
 function addPoint() {
 var objX = document.getElementById("xSelect");
   var xChord = objX.options[objX.selectedIndex].text;
@@ -7,7 +23,16 @@ var objX = document.getElementById("xSelect");
 
   let svg = document.getElementById("frame")
 
-  svg.innerHTML = svg.innerHTML + "<circle cx='" + (50*xChord) + "' cy='" + (500 - 50*yChord) + "' r='10'/>"
+  counter = counter + 1;
+  var objID = "circle-" + counter
+  svg.insertAdjacentHTML("beforeend", "<circle id='" + objID + "' cx='" + (50*xChord) + "' cy='" + (500 - 50*yChord) + "' r='10'/>");
+
+  document.getElementById(objID).addEventListener("click", function(){
+    showID(objID);});
+}
+
+function showID(myID) {
+  document.getElementById("text-div").innerHTML = myID;
 }
 
 let button = document.getElementById("button1")
